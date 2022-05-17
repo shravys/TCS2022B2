2199b44e4be0d25611a15732b70e89e707837024
unknown <shravyab27@gmail.com>
2022-05-17 10:54:48 +0000

    second java prg

---
+++ day2/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+package projectdemo;
+
+public class AddTwoNumbers {
+	public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+        int result = number1 + number2;
+        System.out.println("Sum of two numbers " + result);
+   }
+  }
+  
+
