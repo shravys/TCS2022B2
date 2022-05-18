aadd251f65a9054553c7d2da15568b05b17acd63
unknown <shravyab27@gmail.com>
2022-05-18 09:12:30 +0000

    shravya

---
+++ day2/Addtwonumbers1.java
@@ -0,0 +1,8 @@
+package projectdemo5;
+class Addtwonumbers1 {
+    public static void main(String[] args) {
+	int num1 = 5, num2 = 15, sum;
+	sum = num1 + num2;
+	System.out.println("sum of these numbers:"+sum);
+ }
+}
